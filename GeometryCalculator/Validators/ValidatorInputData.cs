905a1c28e8cf25fb703d790e6f996adb75221608
R451m <rasimg036@mail.com>
2024-09-20 10:53:56 +0300

    --

--- GeometryCalculator/Validators/ValidatorInputData.cs
+++ GeometryCalculator/Validators/ValidatorInputData.cs
@@ -1,18 +1,23 @@
-﻿namespace GeometryCalculator.Validators
+﻿using System.Globalization;
+
+namespace GeometryCalculator.Validators
 {
     public static class ValidatorInputData
     {
         public static double ValidateAndParseToDouble(string input)
         {
+            var epsilon = 0.0001;
             if (string.IsNullOrWhiteSpace(input))
                 throw new ArgumentException($"Ошибка: введено некорректное значение.");
 
-            var size = double.TryParse(input, out double value) ? value : 0;
+            var culture = CultureInfo.GetCultureInfo("ru-RU");
+
+            var size = double.TryParse(input, NumberStyles.Any, culture, out double value) ? value : 0;
 
             if (size < 0)
                 throw new ArgumentException("Размер не может быть отрицательным.");
 
-            if (size == 0)
+            if (size < epsilon)
                 throw new ArgumentException("Размер не может быть равен нулю.");
 
             return size;
